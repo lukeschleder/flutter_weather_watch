a5ab607fb18ab3d5a84b3773ee73d17e191fd614
Luke Schleder <luke.schleder@gmail.com>
2023-04-24 13:25:32 -0500

    reformat

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_weather_watch/screens/weather_app.dart';
 
 void main() async {
-
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
